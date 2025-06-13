93fb98f7f16247408fffafccb4bb5cfc205720e1
KJay08 <kumarjay992991@gmail.com>
2025-06-12 18:36:12 -0600

    Add files via upload

---
+++ C# Projects/ICA/ICA19_SanjayK/ICA19_SanjayK/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace ICA19_SanjayK
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UI_NewPost_Tbx = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.UI_AddPost_Btn = new System.Windows.Forms.Button();
+            this.UI_IdInput_Tbx = new System.Windows.Forms.TextBox();
+            this.UI_DeleteLastPost_Btn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.UI_DeleteById_Btn = new System.Windows.Forms.Button();
+            this.UI_PostedMessage_Lbx = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // UI_NewPost_Tbx
+            // 
+            this.UI_NewPost_Tbx.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.UI_NewPost_Tbx.Location = new System.Drawing.Point(12, 53);
+            this.UI_NewPost_Tbx.Multiline = true;
+            this.UI_NewPost_Tbx.Name = "UI_NewPost_Tbx";
+            this.UI_NewPost_Tbx.Size = new System.Drawing.Size(537, 53);
+            this.UI_NewPost_Tbx.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "New Post";
+            // 
+            // UI_AddPost_Btn
+            // 
+            this.UI_AddPost_Btn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.UI_AddPost_Btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UI_AddPost_Btn.Location = new System.Drawing.Point(574, 65);
+            this.UI_AddPost_Btn.Name = "UI_AddPost_Btn";
+            this.UI_AddPost_Btn.Size = new System.Drawing.Size(141, 32);
+            this.UI_AddPost_Btn.TabIndex = 2;
+            this.UI_AddPost_Btn.Text = "Add Post";
+            this.UI_AddPost_Btn.UseVisualStyleBackColor = true;
+            this.UI_AddPost_Btn.Click += new System.EventHandler(this.UI_AddPost_Btn_Click);
+            // 
+            // UI_IdInput_Tbx
+            // 
+            this.UI_IdInput_Tbx.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.UI_IdInput_Tbx.Location = new System.Drawing.Point(628, 263);
+            this.UI_IdInput_Tbx.Name = "UI_IdInput_Tbx";
+            this.UI_IdInput_Tbx.Size = new System.Drawing.Size(129, 20);
+            this.UI_IdInput_Tbx.TabIndex = 3;
+            this.UI_IdInput_Tbx.TextChanged += new System.EventHandler(this.UI_IdInput_Tbx_TextChanged);
+            // 
+            // UI_DeleteLastPost_Btn
+            // 
+            this.UI_DeleteLastPost_Btn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.UI_DeleteLastPost_Btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UI_DeleteLastPost_Btn.Location = new System.Drawing.Point(628, 161);
+            this.UI_DeleteLastPost_Btn.Name = "UI_DeleteLastPost_Btn";
+            this.UI_DeleteLastPost_Btn.Size = new System.Drawing.Size(129, 42);
+            this.UI_DeleteLastPost_Btn.TabIndex = 4;
+            this.UI_DeleteLastPost_Btn.Text = "Delete Last Post";
+            this.UI_DeleteLastPost_Btn.UseVisualStyleBackColor = true;
+            this.UI_DeleteLastPost_Btn.Click += new System.EventHandler(this.UI_DeleteLastPost_Btn_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(640, 244);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "ID to Delegate";
+            // 
+            // UI_DeleteById_Btn
+            // 
+            this.UI_DeleteById_Btn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.UI_DeleteById_Btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UI_DeleteById_Btn.Location = new System.Drawing.Point(628, 289);
+            this.UI_DeleteById_Btn.Name = "UI_DeleteById_Btn";
+            this.UI_DeleteById_Btn.Size = new System.Drawing.Size(129, 42);
+            this.UI_DeleteById_Btn.TabIndex = 6;
+            this.UI_DeleteById_Btn.Text = "Delete Post By Id";
+            this.UI_DeleteById_Btn.UseVisualStyleBackColor = true;
+            this.UI_DeleteById_Btn.Click += new System.EventHandler(this.UI_DeleteById_Btn_Click);
+            // 
+            // UI_PostedMessage_Lbx
+            // 
+            this.UI_PostedMessage_Lbx.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.UI_PostedMessage_Lbx.FormattingEnabled = true;
+            this.UI_PostedMessage_Lbx.Location = new System.Drawing.Point(12, 161);
+            this.UI_PostedMessage_Lbx.Name = "UI_PostedMessage_Lbx";
+            this.UI_PostedMessage_Lbx.Size = new System.Drawing.Size(537, 264);
+            this.UI_PostedMessage_Lbx.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(9, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(124, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Posted Message";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.UI_PostedMessage_Lbx);
+            this.Controls.Add(this.UI_DeleteById_Btn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.UI_DeleteLastPost_Btn);
+            this.Controls.Add(this.UI_IdInput_Tbx);
+            this.Controls.Add(this.UI_AddPost_Btn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.UI_NewPost_Tbx);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox UI_NewPost_Tbx;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button UI_AddPost_Btn;
+        private System.Windows.Forms.TextBox UI_IdInput_Tbx;
+        private System.Windows.Forms.Button UI_DeleteLastPost_Btn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button UI_DeleteById_Btn;
+        private System.Windows.Forms.ListBox UI_PostedMessage_Lbx;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ C# Projects/ICA/ICA19_SanjayK/ICA19_SanjayK/Form1.cs
@@ -0,0 +1,143 @@
+﻿/* Program     : ICA19
+ * Description : Stacky Posts
+ * Date        : 16 April, 2025
+ * Author      : Sanjay Kumar
+ * Course      : CMPE1666 - Intermediate Programing
+ * Class       : CNTA01
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace ICA19_SanjayK
+{
+    public partial class Form1 : Form
+    {
+        // Declare a Stack to store the messages and other necessary variables
+        Stack<string> msgStk = new Stack<string>(); // Stack to store the messages in reverse order
+        int idCounter = 1; // Counter to keep track of message IDs
+        Stopwatch sw = new Stopwatch(); // Stopwatch to measure elapsed time since the application started
+
+        public Form1()
+        {
+            InitializeComponent(); // Initializes the form components (UI elements)
+
+            this.Text = "ICA 19 - Creating Posts"; // Set the title of the form
+            this.AcceptButton = UI_AddPost_Btn; // Set the default "AcceptButton" to the Add Post button
+
+            sw.Start(); // Start the stopwatch to track the elapsed time
+        }
+
+        // Event handler for when the "Add Post" button is clicked
+        private void UI_AddPost_Btn_Click(object sender, EventArgs e)
+        {
+            if (UI_NewPost_Tbx.Text.Length > 0) // Ensure the textbox is not empty before adding the post
+            {
+                // Create a message with a unique ID, the content of the post, and the time elapsed since the app started
+                string message = $"ID:{idCounter++} - Message : {UI_NewPost_Tbx.Text} - Posted at : {sw.ElapsedMilliseconds / 1000} seconds";
+
+                msgStk.Push(message); // Push the newly created message to the stack
+
+                UI_NewPost_Tbx.Clear(); // Clear the input textbox after the post is added
+
+                DisplayMessage(); // Update the ListBox to show the new list of messages
+            }
+        }
+
+        // Event handler for when the "Delete Last Post" button is clicked
+        private void UI_DeleteLastPost_Btn_Click(object sender, EventArgs e)
+        {
+            if (msgStk.Count > 0) // Check if there are any posts to delete
+            {
+                msgStk.Pop(); // Remove the last post from the stack
+
+                DisplayMessage(); // Update the ListBox to reflect the change
+            }
+        }
+
+        // Event handler for when the "Delete Post by ID" button is clicked
+        private void UI_DeleteById_Btn_Click(object sender, EventArgs e)
+        {
+            if (UI_IdInput_Tbx.Text.Length > 0) // Ensure the user has entered an ID in the textbox
+            {
+                int idToDel;
+                if (int.TryParse(UI_IdInput_Tbx.Text, out idToDel)) // Try parsing the ID from the textbox input
+                {
+                    Stack<string> tempStk = new Stack<string>(); // Temporary stack to hold messages while searching for the ID
+                    bool found = false; // Flag to check if the post with the given ID is found
+
+                    // Loop through the stack to find the message with the given ID
+                    while (msgStk.Count > 0)
+                    {
+                        string message = msgStk.Pop(); // Pop the top message from the stack
+
+                        // Check if the message ID matches the one provided by the user
+                        if (message.StartsWith($"ID:{idToDel}"))
+                        {
+                            found = true; // Set the flag to true if the post is found
+                            break; // Exit the loop once the post is found
+                        }
+                        else
+                        {
+                            tempStk.Push(message); // Push the message to the temporary stack if it doesn't match
+                        }
+                    }
+
+                    // Push all the messages back from the temporary stack to the original stack
+                    while (tempStk.Count > 0)
+                    {
+                        msgStk.Push(tempStk.Pop());
+                    }
+
+                    DisplayMessage(); // Update the ListBox to reflect the change
+
+                    // If the post with the given ID wasn't found, display an error message
+                    if (!found)
+                    {
+                        MessageBox.Show("Post ID not found.");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Invalid number"); // If the entered ID is not a valid number
+                }
+
+                UI_IdInput_Tbx.Clear(); // Clear the ID input textbox after processing
+            }
+        }
+
+        // Method to update the ListBox with the current posts from the stack
+        private void DisplayMessage()
+        {
+            UI_PostedMessage_Lbx.Items.Clear(); // Clear the ListBox before updating it
+
+            // Loop through each message in the stack and add it to the ListBox
+            foreach (string msg in msgStk)
+            {
+                UI_PostedMessage_Lbx.Items.Add(msg); // Add each message to the ListBox
+            }
+        }
+
+        // Event handler for when the user types something in the ID input textbox
+        private void UI_IdInput_Tbx_TextChanged(object sender, EventArgs e)
+        {
+            // If there's text in the ID input textbox, change the AcceptButton to "Delete by ID" button
+            if (UI_IdInput_Tbx.Text.Length > 0)
+            {
+                this.AcceptButton = UI_DeleteById_Btn;
+            }
+            else
+            {
+                // If the ID input textbox is empty, reset the AcceptButton back to the "Add Post" button
+                this.AcceptButton = UI_AddPost_Btn;
+            }
+        }
+    }
+}
